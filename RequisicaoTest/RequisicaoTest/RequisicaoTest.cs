ad79dfbe311838b518b8db693a9dcae2d6c0dee5
Mario-Cesar1 <115711742+Mario-Cesar1@users.noreply.github.com>
2024-01-25 10:24:37 -0300

    a

--- RequisicaoTest/RequisicaoTest/RequisicaoTest.cs
+++ RequisicaoTest/RequisicaoTest/RequisicaoTest.cs
@@ -38,5 +38,48 @@ namespace RequisicaoTest
             driver.Quit();
             Assert.Equal(resultado, veDepartamento);
         }
+
+
+        [Theory]
+        [InlineData("1")]
+        [InlineData("2")]
+        public void CT03R03RequisicaoQtdItem(string valorEsperado)
+        {
+            driver.Navigate().GoToUrl("https://splendorous-starlight-c2b50a.netlify.app/");
+            driver.Manage().Window.Size = new System.Drawing.Size(1022, 718);
+            driver.FindElement(By.Id("CodigoProduto")).Click();
+            driver.FindElement(By.Id("CodigoProduto")).SendKeys("1");
+            driver.FindElement(By.Id("Quantidade")).Click();
+            driver.FindElement(By.Id("Quantidade")).SendKeys(valorEsperado);
+            driver.FindElement(By.CssSelector("#BtnInserirItens > span")).Click();
+            Thread.Sleep(3000);
+            IWebElement tabela = driver.FindElement(By.Id("tabelaItens"));
+            IWebElement celula = tabela.FindElement(By.XPath(".//tr[1]/td[3]"));
+            string valorEncontrado = celula.Text;
+            driver.Quit();
+
+            Assert.Equal(valorEsperado, valorEncontrado);
+        }
+
+        [Theory]
+        [InlineData("1")]
+        [InlineData("2")]
+        public void CT04RequisicaoTotal(string id)
+        {
+            driver.Navigate().GoToUrl("https://splendorous-starlight-c2b50a.netlify.app/");
+            driver.Manage().Window.Size = new System.Drawing.Size(945, 1032);   
+            driver.FindElement(By.Id("CodigoProduto")).Click();
+            driver.FindElement(By.Id("CodigoProduto")).SendKeys(id);
+            driver.FindElement(By.Id("Quantidade")).Click();
+            driver.FindElement(By.Id("Quantidade")).SendKeys(id);
+            driver.FindElement(By.CssSelector("#BtnInserirItens > span")).Click();
+            IWebElement tabela = driver.FindElement(By.Id("tabelaItens"));
+            IWebElement celula = tabela.FindElement(By.XPath($".//tr[{id}]/td[6]"));
+            string valorEsperado = celula.Text;
+            string valorEncontrado = driver.FindElement(By.Id("total")).GetAttribute("value");
+            driver.Quit();
+
+            Assert.Equal(valorEsperado, valorEncontrado);
+        }
     }
 }
